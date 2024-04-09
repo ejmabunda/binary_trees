5923181216a476bb5ef336007b70e5f851dc7e23
Junior Mabunda <mjmabunda0@gmail.com>
2024-04-09 16:12:30 +0200

    Don't link new node to parent

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -16,16 +16,14 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 	if (node)
 	{
 		node->n = value;
-		if (parent)
-		{
-			node->parent = parent;
-			if (parent->left)
-				parent->right = node;
-			else
-				parent->left = node;
-		}
+		node->left = NULL;
+		node->right = NULL;
+		node->parent = parent;
+
 		return (node);
 	}
-
-	return (NULL);
+	else
+	{
+		return (NULL);
+	}
 }
