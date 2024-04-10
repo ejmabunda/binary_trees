322cd4b7e01af4d808f5a56130c491002846a577
Junior Mabunda <mjmabunda0@gmail.com>
2024-04-10 08:57:01 +0200

    Add function that checks if node is root

--- binary_trees.h
+++ binary_trees.h
@@ -50,4 +50,7 @@ void binary_tree_delete(binary_tree_t *tree);
 /* Checks if a node is a leaf */
 int binary_tree_is_leaf(const binary_tree_t *node);
 
+/* Checks if a node is root */
+int binary_tree_is_root(const binary_tree_t *node);
+
 #endif
