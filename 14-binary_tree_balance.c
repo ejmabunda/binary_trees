57f33ed1633fa6c4f1f05e5e6a84d4f29921aa3e
Junior <36537112+ejmabunda@users.noreply.github.com>
2024-04-11 13:38:58 +0200

    Update 14-binary_tree_balance.c

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -16,3 +16,26 @@ int binary_tree_balance(const binary_tree_t *tree)
 	return (binary_tree_height(tree->left) -
 		binary_tree_height(tree->right));
 }
+
+/**
+ * binary_tree_height - Measures the hieght of a binary tree.
+ * @tree: Pointer to the root of the tree.
+ *
+ * Return: The height of a binary tree.
+ */
+size_t binary_tree_height(const binary_tree_t *tree)
+{
+	size_t left_height;
+	size_t right_height;
+
+	if (tree == NULL || (tree->left == NULL && tree->right == NULL))
+		return 0;
+	else
+	{
+		left_height = binary_tree_height(tree->left);
+		right_height = binary_tree_height(tree->right);
+		return max(left_height, right_height) + 1;
+	}
+}
+
+size_t max(size_t a, size_t b) { return (a > b) ? a : b; }
